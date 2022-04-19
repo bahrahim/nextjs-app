c07a50cefce361386b9e83483776410eed0dd138
Rahim BAH <abdoulrahim.bahh@gmail.com>
2022-04-19 09:58:55 +0200

    new component, css update, img loading

---
+++ pages/Experiences.js
@@ -0,0 +1,14 @@
+import Navbar from '../Components/navBar'
+
+
+const Eperiences = () => {
+    return ( 
+        <div>
+            <Navbar />
+            <h1>Experiences</h1>
+            
+        </div>
+     );
+}
+ 
+export default Eperiences;
